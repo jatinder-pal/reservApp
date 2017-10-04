10b73c641d401b3e1aa6d75806bc0477b55c51fb
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-04 18:03:40 +0530

    Update products.php

--- products.php
+++ products.php
@@ -12,15 +12,14 @@ try
 			echo '<form method="post" name="form" id="getproducts" action="#">';
 			echo '<table cellspacing="10" cellpadding="10" border="1">';
 			echo '<thead><tr><th></th><th>Product Name</th><th>Content</th><th>Brand</th><th>Image</th></tr></thead>';
-			echo '<tbody><tr>';
+			echo '<tbody>';
 			foreach($products as $Allproducts) {
-				echo '<td><input type="checkbox" name="product_ids[]" value="'.$Allproducts["id"].'" data-pro-handle="'.$Allproducts["handle"].'" /></td>';
+				echo '<tr><td><input type="checkbox" name="product_ids[]" value="'.$Allproducts["id"].'" data-pro-handle="'.$Allproducts["handle"].'" /></td>';
 				echo '<td>'.$Allproducts['title'].'</td>';
 				echo '<td>'.$Allproducts['body_html'].'</td>';
 				echo '<td>'.$Allproducts['vendor'].'</td>';
-				echo '<td><img src="'.$Allproducts["image"]["src"].'" alt="productimage" /></td>';
+				echo '<td><img src="'.$Allproducts["image"]["src"].'" alt="productimage" /></td></tr>';
 			}
-			echo '</tr>';
 			echo '<tr><td colspan="5"><input type="button" class="saveproducts" value="Show button on Product Page" name="submit" /></td></tr></tbody>';
 			echo '</table>';
 			 echo '</form>';
