0e55b107b81c01175a473a5040acdba80232ab6a
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-04 15:56:50 +0530

    Update products.php

--- products.php
+++ products.php
@@ -9,10 +9,20 @@ try
 	      
 			$products = $shopify('GET /admin/products.json');
 			if($products){
-			echo '<div>';			  
+			echo '<div>';
+			echo '<table>';
+			echo '<thead><tr><th></th><th>Product Name</th><th>Content</th><th>Brand</th><th>Image</th></tr></thead>';
+			echo '<tbody><tr>';
 			foreach($products as $Allproducts) {
 				print_r($Allproducts);
+				echo '<td><input type="checkbox" name="'.$Allproducts->id.'" data-pro-handle="'.$Allproducts->handle.'" /></td>';
+				echo '<td>'.$Allproducts->title.'</td>';
+				echo '<td>'.$Allproducts->body_html.'</td>';
+				echo '<td>'.$Allproducts->vendor.'</td>';
+				echo '<td>'.$Allproducts->image['src'].'</td>';
 			}
+			echo '</tr></tbody>';
+			echo '</table>';
 			 echo '</div>';
 			}
 	else{
