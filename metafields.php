f34e95cd9f18891dd71120e754a038885be0263b
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-04 19:12:22 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -2,12 +2,23 @@
 require __DIR__.'/conf.php'; //Configuration
 require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
-print_r($_REQUEST);
 $access_token = $_REQUEST['access_token'];
+$productids = $_REQUEST['productids'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
-{
-	  echo 'testtt';
+{	
+	foreach($productids as $productid){
+		$metafield = array('metafields' => array(array(
+			'namespace': 'selectedproducts',
+    			'key': 'checked',
+			'value' => $productid,
+			'value_type' => 'string',
+		      )));
+
+		$curl_url = $shopify('POST /admin/products/'.$productid.'/metafields.json', $metafield );
+		print_r($curl_url);
+		echo 'testtt';
+	}
 }
 catch (shopify\ApiException $e)
 {
