3e32d2144b034b9e71b3e4783ac91f003c0a7ce6
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-04 13:52:37 +0530

    Create products.php

---
+++ products.php
@@ -0,0 +1,31 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+$access_token = $_REQUEST['access_token'];
+$order_count = $_REQUEST['order_count'];
+$shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
+try
+{
+	      
+			$products = $shopify('GET /admin/products.json');
+			if($products){
+			echo '<div>';			  
+			foreach($products as $Allproducts)
+			{
+				print_r(Allproducts);
+			}
+			 echo '</div>';
+			}
+	else{
+	echo "<div class='no-result'>No Products</div>";
+	}
+}
+catch (shopify\ApiException $e)
+{
+	# HTTP status code was >= 400 or response contained the key 'errors'
+	echo $e;
+	print_r($e->getRequest());
+	print_r($e->getResponse());
+}
+?>
