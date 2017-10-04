9285dc7bf172b428829efc6098fd35220e42f73b
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-04 13:21:06 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
        
-	define('SHOPIFY_APP_API_KEY', '890b135f9389701022b967f1025c84c3');
-	define('SHOPIFY_APP_SHARED_SECRET', '91e597828618ffb34c354cccb064cc04');
+	define('SHOPIFY_APP_API_KEY', 'e54beaa8ca92d08d6519ad835300ba0a');
+	define('SHOPIFY_APP_SHARED_SECRET', '821bdc7bdd58060e2caffffb7a7f1221');
 	
 ?>
 
