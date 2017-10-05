9900c3a00585c7e16b9117a9b62efba049ecca9e
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-05 13:53:41 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -4,19 +4,16 @@ require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
 $alloptions = $_REQUEST['options'];
-$alloptions = explode(',', $alloptions);
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
-{	
-	foreach($alloptions as $option){	
-		$metafield = array( "metafield" => array('namespace' => 'selectedoptions', 'key' => 'seloptions', 'value' => $option,
+{		
+		$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'seloptions', 'value' => $alloptions,
 		'value_type' => 'string'));
 		$response = $shopify('POST /admin/metafields.json',$metafield);
 		print_r($response);
 		foreach($response as $response_option){
 		  echo $response_option;
 		}
-	}
 }
 catch (shopify\ApiException $e)
 {
