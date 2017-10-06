0ca55b7bd58e8ab93a9b4f15ce109fef201afdd9
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-06 13:26:37 +0530

    Update addRevise.js

--- addRevise.js
+++ addRevise.js
@@ -4,10 +4,11 @@ alert(123);
 var data = $("script[src*='addRevise.js']").attr('src').split('?')[1];
 $.ajax({
   url: 'https://revise-app.herokuapp.com/getmetafields.php?'+data,
-  success: function(data){
-      alert(data);
-      data = data.split(',');
+  success: function(response){
+      alert(response);
+      data = response.split(',');
       $.each(data, function(index, value){
+        alert(value);
         var url = window.location.href;
         if(url.indexOf('/products/') > -1 && value == 'product_page'){
           if($('.add_to_cart').length){
