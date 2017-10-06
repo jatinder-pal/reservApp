066cc8f546ac5323cce9de9c555f0c9961db5e45
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-06 12:14:02 +0530

    Update addScript.php

--- addScript.php
+++ addScript.php
@@ -7,15 +7,15 @@ $alloptions = $_REQUEST['options'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {
-	$js_file = 'https://revise-app.herokuapp.com/addRevise.js';
+	$js_file = 'https://revise-app.herokuapp.com/addRevise.js&id='.$access_token;
 	if($alloptions == 'noData'){
-		$data = $shopify('GET /admin/script_tags.json?src=https://revise-app.herokuapp.com/addRevise.js');
+		$data = $shopify('GET /admin/script_tags.json?src=https://revise-app.herokuapp.com/addRevise.js&id='.$access_token);
 		foreach($data as $file){
 			$response = $shopify('DELETE /admin/script_tags/'.$file['id'].'.json');
 			print_r('Remove JS file');
 		}
 	} else {
-		$data = $shopify('GET /admin/script_tags.json?src=https://revise-app.herokuapp.com/addRevise.js');
+		$data = $shopify('GET /admin/script_tags.json?src=https://revise-app.herokuapp.com/addRevise.js&id='.$access_token);
 		if(!$data){
 			$fields = array( "script_tag" => array('event' => 'onload', 'src' => $js_file));
 			$response = $shopify('POST /admin/script_tags.json',$fields);
