9c39d2449b4a9228b5d85d040c2b72c4fef40d98
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-06 11:11:58 +0530

    Update addRevise.js

--- addRevise.js
+++ addRevise.js
@@ -1,1 +1,7 @@
-/****************addRevise.js********************/
+<!--Revise.js-->
+var url = window.location.href;
+if(url.indexOf('/products/') > -1){
+  if($('. add_to_cart').length){
+    $('. add_to_cart').after('<p>testinggggg</p>');
+  }
+}
