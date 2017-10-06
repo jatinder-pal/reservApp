fb7eed6e6465b316df68f2d6a47d2ea8aa121d67
boski-mehta <boskim.3ginfio@gmail.com>
2017-10-06 13:13:58 +0530

    Update addScript.php

--- addScript.php
+++ addScript.php
@@ -8,7 +8,7 @@ $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token 
 try
 {
 	$url = "/admin/script_tags.json?src=https://revise-app.herokuapp.com/addRevise.js?id=$access_token";
-	$fields = "https://revise-app.herokuapp.com/addRevise.js?id=$access_token";
+	$js_file = "https://revise-app.herokuapp.com/addRevise.js?id=$access_token";
 	if($alloptions == 'noData'){
 		$data = $shopify("GET $url");
 		foreach($data as $file){
