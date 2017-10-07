11250e2a1b9a31d2245f35701c944e37fc5c4e29
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-07 11:51:53 +0530

    Update metafields.php

--- metafields.php
+++ metafields.php
@@ -4,33 +4,19 @@ require __DIR__.'/vendor/autoload.php';
 use phpish\shopify;
 $access_token = $_REQUEST['access_token'];
 $alloptions = $_REQUEST['options'];
-$allclasses = $_REQUEST['classes'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {	
 	if($alloptions){
 	$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'seloptions', 'value' => $alloptions,
 	'value_type' => 'string'));
-		if($allclasses) {
-		$classfield = array( "metafield" => array('namespace' => 'reviseclass', 'key' => 'selclasses', 'value' => $allclasses,
-	'value_type' => 'string'));
-		} else {
-		$allclasses = "noClass";
-		$classfield = array( "metafield" => array('namespace' => 'reviseclass', 'key' => 'selclasses', 'value' => $allclasses,
-	'value_type' => 'string'));
-		}
 	} else {
 	$alloptions = "noData";
 	$metafield = array( "metafield" => array('namespace' => 'revisebutton', 'key' => 'seloptions', 'value' => $alloptions,
 	'value_type' => 'string'));
 	}
-	$response1 = $shopify('POST /admin/metafields.json',$metafield);
-	$response2 = $shopify('POST /admin/metafields.json',$classfield);
-	//print_r($response);
-	$array = array( $response1['value'] => $response2['value']);
-	echo json_encode($array);
-	//echo $response1['value'];
-	//echo $response2['value'];
+	$response = $shopify('POST /admin/metafields.json',$metafield);
+	echo $response['value'];
 }
 catch (shopify\ApiException $e)
 {
