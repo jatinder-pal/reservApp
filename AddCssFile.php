0159c0c92892cb5eb51f1a3ce0c5595f7085b066
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-09 15:55:29 +0530

    Update AddCssFile.php

--- AddCssFile.php
+++ AddCssFile.php
@@ -24,9 +24,9 @@ try
 		  
 		$themefile = $shopify('GET /admin/themes/'.$theme['id'].'/assets.json?asset[key]=layout/theme.liquid&theme_id='.$theme['id']);
 		$myfile = $themefile['value'];
-		$splitfile = explode("</head>", $myfile);
+		$splitfile = explode("{{ content_for_header }}", $myfile);
 		//$themehtml = $splitfile[0].'{{ "custom_reserve.css" | asset_url | stylesheet_tag }} </head>'.$splitfile[1];
-		$themedata = array( "asset" => array('key' => 'layout/theme.liquid', 'value' => $splitfile[0].'{{ "custom_reserve.css" | asset_url | stylesheet_tag }} </head>'.$splitfile[1] ));
+		$themedata = array( "asset" => array('key' => 'layout/theme.liquid', 'value' => $splitfile[0].'{{ content_for_header }}{{ "custom_reserve.css" | asset_url | stylesheet_tag }}'.$splitfile[1] ));
 		$newthemefile = $shopify('PUT /admin/themes/'.$theme['id'].'/assets.json',$themedata);
 		print_r($newthemefile);
 		
