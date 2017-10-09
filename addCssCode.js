6dadf1c87e8a800acab308315a3729a4eee30b84
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-09 17:31:16 +0530

    Update addCssCode.js

--- addCssCode.js
+++ addCssCode.js
@@ -1,5 +1,13 @@
 <!--Add CSS Code-->
 (function(){
-var data = $("script[src*='addCssCode.js']").attr('src').split('?')[1]; 
- console.log(data);
+var data = $("script[src*='addRevise.js']").attr('src').split('?')[1]; 
+$.ajax({
+  crossDomain: true,
+  url: 'https://reserv-app.herokuapp.com/getCss_json.php?'+data,
+  dataType: "jsonp",
+  header: {"Access-Control-Allow-Origin": "https://sendd-shipping.myshopify.com"},
+  success: function(response){
+      console.log(response);
+  }
+});
 })();
