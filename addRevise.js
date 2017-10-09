c4f072dd1d8d1c92cbf031c27de4783864f7e3e8
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-09 11:03:37 +0530

    Update addRevise.js

--- addRevise.js
+++ addRevise.js
@@ -3,7 +3,7 @@
 var data = $("script[src*='addRevise.js']").attr('src').split('?')[1]; 
 $.ajax({
   crossDomain: true,
-  url: 'https://revise-app.herokuapp.com/getmeta_json.php?'+data,
+  url: 'https://reserv-app.herokuapp.com/getmeta_json.php?'+data,
   dataType: "jsonp",
   header: {"Access-Control-Allow-Origin": "https://sendd-shipping.myshopify.com"},
   success: function(response){
