c949f48feb70b06c8d80fdb1e420859b5ddbdea5
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-09 16:23:18 +0530

    Create getCSSfile.php

---
+++ getCSSfile.php
@@ -0,0 +1,25 @@
+<?php
+require __DIR__.'/conf.php'; //Configuration
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+$access_token = $_REQUEST['access_token'];
+$cssCode = $_REQUEST['cssCode'];
+$shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
+try
+{	
+	$themes = $shopify('GET /admin/themes.json');
+	foreach($themes as $theme){
+	  if($theme['role'] == 'main') {
+		  $response = $shopify('GET /admin/themes/'.$theme['id'].'/assets.json?asset[key]=assets/custom_reserve.css&theme_id='.$theme['id']);
+      print_r($response);
+	  }
+	}
+}
+catch (shopify\ApiException $e)
+{
+	# HTTP status code was >= 400 or response contained the key 'errors'
+	echo $e;
+	print_r($e->getRequest());
+	print_r($e->getResponse());
+}
+?>
