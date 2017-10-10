88efeea5d8332b0c599f32ff1eb15293d1f2404d
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-10 11:13:05 +0530

    Update addScript.php

--- addScript.php
+++ addScript.php
@@ -7,26 +7,33 @@ $alloptions = $_REQUEST['options'];
 $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
 try
 {
+	$alloptions = explode('===',$alloptions);
+	$alloptions = $alloptions[0];
+	$auto_manual = $alloptions[1];
 	$url = "/admin/script_tags.json?src=https://reserv-app.herokuapp.com/addReserv.js?access_token=$access_token";
 	$js_file = "https://reserv-app.herokuapp.com/addReserv.js?access_token=$access_token";
-	if($alloptions == 'noData'){
-		$data = $shopify("GET $url");
-		foreach($data as $file){
-			//print_r($file);
-			$response = $shopify('DELETE /admin/script_tags/'.$file['id'].'.json');
-			print_r('Remove JS file');
-		}
-	} else {
-		$data = $shopify("GET $url");
-		if(!$data){
-			$fields = array( "script_tag" => array('event' => 'onload', 'src' => $js_file));
-			$response = $shopify('POST /admin/script_tags.json',$fields);
-			//print_r($response);
-			print_r('Add JS file');
+	if($auto_manual == 'automatic_code') {
+		if($alloptions == 'noData'){
+			$data = $shopify("GET $url");
+			foreach($data as $file){
+				//print_r($file);
+				$response = $shopify('DELETE /admin/script_tags/'.$file['id'].'.json');
+				print_r('Remove JS file');
+			}
 		} else {
-			//print_r($data);
-			print_r('Already exist JS file');
+			$data = $shopify("GET $url");
+			if(!$data){
+				$fields = array( "script_tag" => array('event' => 'onload', 'src' => $js_file));
+				$response = $shopify('POST /admin/script_tags.json',$fields);
+				//print_r($response);
+				print_r('Add JS file');
+			} else {
+				//print_r($data);
+				print_r('Already exist JS file');
+			}
 		}
+	} else if($auto_manual == 'manual_code') {
+		echo "https://reserv-app.herokuapp.com/addReserv.js?access_token=$access_token&shop=".$_REQUEST['shop'];
 	}
 }
 catch (shopify\ApiException $e)
