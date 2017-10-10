3d6a4e27e48c95e015ad548fb22462767dbdd792
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-10 16:23:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script src="https://use.fontawesome.com/988a7dc35f.js"></script>
 	<link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"  rel="stylesheet" type="text/css"/>  
-	<link href="style.css" rel="stylesheet" type="text/css"/>
+	<link href="css/style.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
 <h2>Hello welcome to my app</h2>
