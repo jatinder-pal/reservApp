f1f57a4e428ef8f66599bef59d45ec825294617f
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-11 12:56:30 +0530

    Update addCssCode.js

--- addCssCode.js
+++ addCssCode.js
@@ -1,6 +1,6 @@
 <!--Add CSS Code-->
 (function(){
-var data = $("script[src*='addRevise.js']").attr('src').split('?')[1]; 
+var data = $("script[src*='addCssCode.js']").attr('src').split('?')[1]; 
 $.ajax({
   crossDomain: true,
   url: 'https://reserv-app.herokuapp.com/getCss_json.php?'+data,
