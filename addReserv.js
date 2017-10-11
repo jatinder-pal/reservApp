f795c0a551f011664eeb0b8491b6b7ff1c3c4074
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-11 12:01:33 +0530

    Update addReserv.js

--- addReserv.js
+++ addReserv.js
@@ -5,7 +5,7 @@ $.ajax({
   crossDomain: true,
   url: 'https://reserv-app.herokuapp.com/getmeta_json.php?'+data,
   dataType: "jsonp",
-  header: {"Access-Control-Allow-Origin": "https://sendd-shipping.myshopify.com"},
+  header: {"Access-Control-Allow-Origin": "*"},
   success: function(response){
       //console.log(response['options']);
       var data = response['options'];
@@ -18,14 +18,32 @@ $.ajax({
         if(url.indexOf('/products/') > -1 && value == 'product_page'){
           if($('.'+classes).length){
 	    var product_url = window.location.href+'.json';
-	    console.log(product_url);  
-            $('.'+classes).after('<a href="#" class="reserv_button">RESERV - The New Layaway</a>');
+	    console.log(product_url); 
+		$.ajax({
+		crossDomain: true,
+		url: product_url,
+		dataType: "jsonp",
+		header: {"Access-Control-Allow-Origin": "*"},
+		success: function(response){
+			console.log(response);
+		}
+		});
+            $('.'+classes).after('<a href="#" class="reserv_button">RESERV <br/><span>The New Layaway</span></a>');
           }
         } else if(url.indexOf('/collections/') > -1 && url.indexOf('/products/') === -1 && value == 'catalog_page'){
           if($('.'+classes).length){
 	    var collection_url = window.location.href+'/products.json';
 	    console.log(collection_url);
-            $('.'+classes).after('<a href="#" class="reserv_button">RESERV - The New Layaway</a>');
+		$.ajax({
+		crossDomain: true,
+		url: collection_url,
+		dataType: "jsonp",
+		header: {"Access-Control-Allow-Origin": "*"},
+		success: function(response){
+			console.log(response);
+		}
+		});
+            $('.'+classes).after('<a href="#" class="reserv_button">RESERV <br/><span>The New Layaway</span></a>');
           }
         }
         
