7965c81161e533c5df270a4b53e2f3de5c16080b
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-11 17:44:44 +0530

    Update addReserv.js

--- addReserv.js
+++ addReserv.js
@@ -73,10 +73,12 @@ $.ajax({
 			}
 			var link = 'id='+id+'&name='+name+'&image='+image+'&description='+desc+'&price='+price+'&shipping='+shipping+'&tax='+tax;
 			console.log(link);
-			console.log(index);
-			if(index == 1){
+			var count = index + 1;
+			if(count == 1){
+			alert($('body .'+classes+':nth-child('+index+')'));
 			$('body .'+classes+':first-child').after('<a href="'+link+'" class="reserv_button">RESERV<br/><span>The New Layaway</span></a>');
 			} else{
+			alert($('body .'+classes+':nth-child('+index+')'));
 			$('body .'+classes+':nth-child('+index+')').after('<a href="'+link+'" class="reserv_button">RESERV<br/><span>The New Layaway</span></a>');
 			}
 			});
