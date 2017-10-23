923fdba8b79cece5fdf3661c2381088d4e64f3c9
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-23 16:58:53 +0530

    Create saveMerchantApi.php

---
+++ saveMerchantApi.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__.'/conf.php'; //Configuration
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+$access_token = $_REQUEST['access_token'];
+$merchantId = $_REQUEST['merchantId'];
+$shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
+try
+{	
+	print_r($merchantId);
+}
+catch (shopify\ApiException $e)
+{
+	# HTTP status code was >= 400 or response contained the key 'errors'
+	echo $e;
+	print_r($e->getRequest());
+	print_r($e->getResponse());
+}
+?>
