e2ff899e5838bf3fc20eb8b716731584c768c57f
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-23 13:29:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $access_token = shopify\access_token($_REQUEST['shop'], SHOPIFY_APP_API_KEY, SHO
 </head>
 <body>
 <div class="logo">
-	<img class="logo_img" src="images/ReservStoreLogo.jpg" alt="ReservStoreLogo.jpg" />
+	<img class="logo_img" src="images/ReservTheNewLayawayLogo.jpg" alt="ReservStoreLogo" />
 </div>
 <div class="content-container">
 <div id="tabs">
