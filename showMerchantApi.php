111708101fe5be6e7bd90489299b300c00200669
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-24 11:51:21 +0530

    Create showMerchantApi.php

---
+++ showMerchantApi.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__.'/conf.php'; //Configuration
+require __DIR__.'/vendor/autoload.php';
+use phpish\shopify;
+$access_token = $_REQUEST['access_token'];
+$shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token );
+try
+{	
+	$response = $shopify('GET /admin/metafields.json');
+	foreach($response as $options){
+		if($options['namespace'] == 'genarateMerchantId'){
+			echo $getMerchantId = $options['value'];
+		}
+	}
+}
+catch (shopify\ApiException $e)
+{
+	# HTTP status code was >= 400 or response contained the key 'errors'
+	echo $e;
+	print_r($e->getRequest());
+	print_r($e->getResponse());
+}
+?>
