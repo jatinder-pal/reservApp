aec4bcd0ba1044f94412863f840e44a422bfe240
jatinder-pal <boskim.3ginfio@gmail.com>
2017-10-24 12:05:30 +0530

    Update showMerchantApi.php

--- showMerchantApi.php
+++ showMerchantApi.php
@@ -7,10 +7,15 @@ $shopify = shopify\client($_REQUEST['shop'], SHOPIFY_APP_API_KEY, $access_token 
 try
 {	
 	$response = $shopify('GET /admin/metafields.json');
+	$flag = false;
 	foreach($response as $options){
 		if($options['namespace'] == 'genarateMerchantId'){
+			$flag = true;
 			echo $getMerchantId = $options['value'];
 		}
+	}
+	if($flag == false){
+		echo 'No MerchantID';
 	}
 }
 catch (shopify\ApiException $e)
