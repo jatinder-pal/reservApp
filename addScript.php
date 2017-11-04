affd17c612ac6cded25d0227d1cb4a43d391c92e
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-04 15:49:58 +0530

    Update addScript.php

--- addScript.php
+++ addScript.php
@@ -12,8 +12,8 @@ try
 	$auto_manual = $alloptions[1];
 	$alloptions = $alloptions[0];
 	echo $server = 'https://'.$server;
-	$url = "/admin/script_tags.json?src=$server/addReserv.js?access_token=$access_token";
-	$js_file = "$server/addReserv.js?access_token=$access_token";
+	$url = '/admin/script_tags.json?src=$server/addReserv.js?access_token=$access_token';
+	$js_file = '$server/addReserv.js?access_token=$access_token';
 	if($auto_manual == 'automatic_code') {
 		if($alloptions == 'noData'){
 			$data = $shopify("GET $url");
