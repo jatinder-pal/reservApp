4fc5aa2784f28698f4e656c4e98074de5f01e746
jatinder-pal <boskim.3ginfio@gmail.com>
2017-11-04 10:35:12 +0530

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php
        
-	define('SHOPIFY_APP_API_KEY', 'e54beaa8ca92d08d6519ad835300ba0a');
-	define('SHOPIFY_APP_SHARED_SECRET', '821bdc7bdd58060e2caffffb7a7f1221');
+	define('SHOPIFY_APP_API_KEY', '32cd6068a4829195aa3a70fdc542bf59');
+	define('SHOPIFY_APP_SHARED_SECRET', '808d600164f8671a85349f0bbe5eb1ea');
 	
 ?>
 
